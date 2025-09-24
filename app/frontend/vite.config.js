5ee6cb7e97eaaa8701f4ac40c3346a86af77ad4c
ttembo <thulani.tembo@betika.com>
2025-09-25 01:19:51 +0200

    fixing undefined issue

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -5,7 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   },
   preview: {
     host: '0.0.0.0',
